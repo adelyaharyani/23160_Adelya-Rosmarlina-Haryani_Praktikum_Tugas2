4ffb1ed098856d82fcb5c379334858fb171f3a6e
adelyaharyani <152018694+adelyaharyani@users.noreply.github.com>
2024-09-09 22:58:50 +0700

    Add files via upload

---
+++ Bingo.java
@@ -0,0 +1,39 @@
+public class Bingo {
+    private String name;
+
+    public Bingo(String name) {
+        this.name = name;
+    }
+
+    public void sing() {
+        // Menampilkan awal lirik lagu
+        // Menggunakan refrain untuk mempermudah & keteraturan kode
+        String refrain = "There was a farmer who had a dog,\nAnd Bingo was his name-o.\n";
+
+        // Looping untuk menghilangkan huruf B-I-N-G-O satu per satu
+        for (int clapCount = 0; clapCount <= 5; clapCount++) {
+            // Menampilkan bagian awal lirik
+            System.out.print(refrain);
+
+            // Menampilkan "clap" sesuai iterasi
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < clapCount; j++) {
+                    System.out.print("(clap)-");
+                }
+                // Menampilkan huruf yang tersisa setelah clap
+                System.out.println(name.substring(clapCount));
+            }
+
+            // Menampilkan akhir lirik
+            System.out.println("And Bingo was his name-o.\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Bingo bingo = new Bingo("BINGO");
+
+        // Method sing untuk menyanyikan lagu
+        bingo.sing();
+    }
+}
+
